c17aef4ba5ed4753f8968fb8a7fb8ad0d8bb6ccf
Firahs <firahs.chebbo@utoronto.ca>
2021-12-07 14:32:27 -0800

    Initial checkin

---
+++ src/main/java/fc/awesome/phaseBot/PhaseBotApplication.java
@@ -0,0 +1,13 @@
+package fc.awesome.phaseBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fc/awesome/phaseBot/PhaseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.awesome.phaseBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
